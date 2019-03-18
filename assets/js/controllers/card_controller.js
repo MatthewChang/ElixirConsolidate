ccc32d6077b9c7a7692ac609af23831b7392a7d4
MatthewChang <mjchang07@gmail.com>
2019-03-18 17:48:36 -0400

    card flipping done

--- assets/js/controllers/card_controller.js
+++ assets/js/controllers/card_controller.js
@@ -1,9 +1,23 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "question", "answer" ]
+  static targets = [ "front", "back" ]
 
   flip() {
-    alert('here')
+    this.flipped = !this.flipped
+    if(this.flipped) {
+      this.frontTarget.classList.toggle("hidden",true)
+      this.backTarget.classList.toggle("hidden",false)
+    } else {
+      this.frontTarget.classList.toggle("hidden",false)
+      this.backTarget.classList.toggle("hidden",true)
+    }
+  }
+
+  get flipped() {
+    return this.data.get('flipped') == 'true'
+  }
+  set flipped(val) {
+    this.data.set('flipped',val ? 'true' : 'false')
   }
 }
